460a840a227fac834511c5d446c097de98a29e1c
EmanuelDavid <mobile20ema@gmail.com>
2023-08-18 00:57:40 -0300

    UI y algo de codigo
    integrando de a poco codigo para la ball y las paddle

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public float startSpeed = 5f; // Velocidad inicial de la pelota
-    public float speedIncreaseFactor = 1.1f; // Factor de aumento de velocidad al rebotar
-    public Vector2 startDirection = new Vector2(1f, 0f); // Dirección inicial de la pelota
 
     private Rigidbody2D rb;
 
@@ -22,14 +20,21 @@ public class BallController : MonoBehaviour
 
     public void StartBall()
     {
-        rb.velocity = startDirection.normalized * startSpeed; // Iniciar la pelota con la velocidad y dirección iniciales
+        // Establecer velocidad inicial y dirección aleatoria
+        rb.velocity = new Vector2(startSpeed, Random.Range(-startSpeed, startSpeed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Cambiar la dirección de la pelota al rebotar en la raqueta o en los bordes del área de juego
-        Vector2 newDirection = Vector2.Reflect(rb.velocity.normalized, collision.contacts[0].normal);
-        rb.velocity = newDirection * startSpeed * speedIncreaseFactor;
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            // Cambiar dirección horizontal y aumentar velocidad
+            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y) * 1.05f;
+        }
+        else
+        {
+            // Cambiar la dirección de la pelota al rebotar en los bordes del área de juego
+            rb.velocity = Vector2.Reflect(rb.velocity, collision.contacts[0].normal) * 1.05f;
+        }
     }
 }
-
